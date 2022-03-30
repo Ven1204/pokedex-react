052ab70a7b388466112b78fb683c0721b2076d6b
Rayven <rayven.rey@gmail.com>
2022-03-31 01:11:51 +0200

    rendering names

--- src/components/pokemons/pokemon.js
+++ src/components/pokemons/pokemon.js
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// const pokemonImages = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{}.png"
+// const pokemonID = 'https://pokeapi.co/api/v2/pokemon/1'
+// const pokemonList = ''https://pokeapi.co/api/v2/pokemon?limit=151''
+
 function Pokemon(){
   const [data, setData] = useState([]);
 
- useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-		axios.get('https://pokeapi.co/api/v2/pokemon?limit=99')
+  useEffect(() => {
+		axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
     .then(response => {
       var pokeData = [];
       pokeData = response.data.results
+
       setData(pokeData);
       console.log(pokeData)
+      })
+      .catch((error) => {
+        console.log(error);
     });
-  }
+  }, []);
 
 
   return (
     <div>
       <h1>asdasdsa</h1>
-      {data.map(pokemon =>{(
-        <h1>{pokemon.name}</h1>
-      )})}
+      {data.map(pokemon => (
+        <div>
+
+          <h2># {}</h2>
+          <h1>{pokemon.name}</h1>
+        </div>
+
+      ))}
     </div>
   )
 }
