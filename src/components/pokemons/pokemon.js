a0c76753773d89629f5c4990ad5404196f32440d
Rayven <rayven.rey@gmail.com>
2022-03-31 17:43:00 +0200

    rendering images

--- src/components/pokemons/pokemon.js
+++ src/components/pokemons/pokemon.js
@@ -1,39 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// const pokemonImages = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{}.png"
-// const pokemonID = 'https://pokeapi.co/api/v2/pokemon/1'
-// const pokemonList = ''https://pokeapi.co/api/v2/pokemon?limit=151''
+// to get the 1-151 numbers and interpolate in imageAPI
+let x = 1
+
+const imageAPI = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${x}.png`);
+// imageAPI.repeat(10);
+const pokemonImages = Array.apply(null, Array(151)).map(_ => imageAPI)
+// console.log(pokemonImages)
+
+const l = pokemonImages.forEach(element => {
+  return element
+});
+
+
+
+const pokemonID = ('https://pokeapi.co/api/v2/pokemon/1');
+const pokemonList = ('https://pokeapi.co/api/v2/pokemon?limit=151');
 
 function Pokemon(){
   const [data, setData] = useState([]);
 
   useEffect(() => {
-		axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => {
-      var pokeData = [];
-      pokeData = response.data.results
+    getName();
+    // getImage();
+  }, []);
 
-      setData(pokeData);
-      console.log(pokeData)
+    const getImage = () => {
+    axios.get(imageAPI)
+    // .then(response => {
+    //   var pokeImage = [];
+    //   pokeImage = response.data.results
+
+    //   setData(pokeImage);
+    //   console.log(pokeImage)
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    // });
+    // console.log(pokemonImages)
+  }
+  const getName = () => {
+    axios.get(pokemonList)
+    .then(response => {
+      var pokeName = [];
+      pokeName = response.data.results
+
+      setData(pokeName);
+      // console.log(pokeName)
       })
       .catch((error) => {
         console.log(error);
     });
-  }, []);
+  }
 
 
   return (
     <div>
-      <h1>asdasdsa</h1>
+      {pokemonImages.map(image => (
+        <img src={image} alt="asd"/>
+      ))}
       {data.map(pokemon => (
-        <div>
-
+        <div key={pokemon.name}>
           <h2># {}</h2>
           <h1>{pokemon.name}</h1>
         </div>
 
       ))}
+
     </div>
   )
 }
